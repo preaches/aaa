aa00ed5b44619f191003c4772026cc71c2ce417d
preaches <preachingasf@protonmail.com>
2024-12-16 12:37:09 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,11 @@
 import tkinter as tk
-import ctypes, json
+import ctypes
+import json
 import time
 import threading
-import win32api
 import socket
 import pickle
+from pynput import mouse, keyboard  # Cross-platform mouse and keyboard library
 
 with open(r"config.json") as json_file:
     data = json.load(json_file)
@@ -18,21 +19,31 @@ class FullScreenApp:
         self.root.attributes("-fullscreen", True)
         self.root.configure(background='white')
         self.root.bind('<Motion>', self.on_mouse_motion)
+        
         self.screen_width = self.root.winfo_screenwidth()
         self.screen_height = self.root.winfo_screenheight()
         self.center_x = self.screen_width // 2
         self.center_y = self.screen_height // 2
         self.prev_x = self.center_x
         self.prev_y = self.center_y
+        
         self.mouse_movement = {"x": 0, "y": 0}
         self.mouse_button = None
         self.mouse5_state = False
         self.mouse6_state = False
         self.running = True
+        
         self.host = host
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((self.host, self.port))
+        
+        # Setup mouse and keyboard listeners
+        self.mouse_listener = mouse.Listener(on_move=self.on_mouse_move)
+        self.keyboard_listener = keyboard.Listener(
+            on_press=self.on_press, 
+            on_release=self.on_release
+        )
 
     def on_mouse_motion(self, event):
         rel_x = event.x - self.prev_x
@@ -46,65 +57,45 @@ class FullScreenApp:
             rel_x = rel_y = 0  # resets mouse_movement (just in case)
         self.mouse_movement = {"x": rel_x, "y": rel_y}
 
-    
-    def get_button_state(self): #records all button states except 5/6 they have their own thread as we handle them seperately for the cheese :) (driver doesnt support these)
-        while self.running:
-            current_key = None
-            if win32api.GetAsyncKeyState(0x01) < 0:
-                current_key = "Left"
-            elif win32api.GetAsyncKeyState(0x02) < 0:
-                current_key = "Right"
-            elif win32api.GetAsyncKeyState(0x04) < 0:
-                current_key = "Middle"
-                
-            if current_key:
-                self.mouse_button = current_key
-            elif current_key == None:
-                self.mouse_button = current_key
-            time.sleep(0.0001)
+    def on_mouse_move(self, x, y):
+        # This method is called by pynput mouse listener
+        rel_x = x - self.prev_x
+        rel_y = y - self.prev_y
 
-    def check_mouse5(self):
-        while self.running:
-            if win32api.GetAsyncKeyState(0x05) < 0:
+        max_movement = 256
+        rel_x = max(min(rel_x, max_movement), -max_movement)
+        rel_y = max(min(rel_y, max_movement), -max_movement)
+
+        self.mouse_movement = {"x": rel_x, "y": rel_y}
+        self.prev_x, self.prev_y = x, y
+
+    def on_press(self, key):
+        try:
+            # Check mouse buttons
+            if key == mouse.Button.left:
+                self.mouse_button = "Left"
+            elif key == mouse.Button.right:
+                self.mouse_button = "Right"
+            elif key == mouse.Button.middle:
+                self.mouse_button = "Middle"
+            
+            # Check additional mouse buttons
+            if key == mouse.Button.x1:
                 self.mouse5_state = True
-            else:
-                self.mouse5_state = False
-            time.sleep(0.001)
-
-    def check_mouse6(self):
-        while self.running:
-            if win32api.GetAsyncKeyState(0x06) < 0:
+            elif key == mouse.Button.x2:
                 self.mouse6_state = True
-            else:
-                self.mouse6_state = False
-            time.sleep(0.001)
+        except AttributeError:
+            pass
 
-    def reset_mouse_position(self):
-        ctypes.windll.user32.SetCursorPos(self.center_x, self.center_y)
-    
-    def data_received_handler(self):
-        while self.running:
-            data = self.socket.recv(1024)
-            if data:
-                received_data = pickle.loads(data)  # Deserialize data and reset the position
-                print("Received data:", received_data)
-                self.reset_mouse_position()
-
-    def run(self):
-        mouse5_thread = threading.Thread(target=self.check_mouse5)
-        mouse6_thread = threading.Thread(target=self.check_mouse6)
-        data_received_thread = threading.Thread(target=self.data_received_handler)
-        left_click_thread = threading.Thread(target=self.get_button_state)
-        mouse5_thread.start()
-        mouse6_thread.start()
-        left_click_thread.start()
-
-        print_thread = threading.Thread(target=self.print_state)
-        print_thread.start()
-        data_received_thread.start()
-
-        self.root.mainloop()
-        self.running = False
+    def on_release(self, key):
+        # Reset button states on release
+        if key in [mouse.Button.left, mouse.Button.right, mouse.Button.middle]:
+            self.mouse_button = None
+        
+        if key == mouse.Button.x1:
+            self.mouse5_state = False
+        elif key == mouse.Button.x2:
+            self.mouse6_state = False
 
     def print_state(self): 
         last_state = None
@@ -126,12 +117,40 @@ class FullScreenApp:
 
             self.mouse_movement = {"x": 0, "y": 0}
             time.sleep(tcp_sleep) #a sleep to stabilize
-    
-    
 
+    def reset_mouse_position(self):
+        # Cross-platform mouse reset
+        mouse_controller = mouse.Controller()
+        mouse_controller.position = (self.center_x, self.center_y)
+
+    def data_received_handler(self):
+        while self.running:
+            data = self.socket.recv(1024)
+            if data:
+                received_data = pickle.loads(data)  # Deserialize data and reset the position
+                print("Received data:", received_data)
+                self.reset_mouse_position()
+
+    def run(self):
+        # Start listeners
+        self.mouse_listener.start()
+        self.keyboard_listener.start()
+
+        # Start threads
+        data_received_thread = threading.Thread(target=self.data_received_handler)
+        print_thread = threading.Thread(target=self.print_state)
+        
+        print_thread.start()
+        data_received_thread.start()
+
+        self.root.mainloop()
+        
+        # Stop listeners when done
+        self.running = False
+        self.mouse_listener.stop()
+        self.keyboard_listener.stop()
 
 if __name__ == "__main__":
-
     remote_host = ip  
     remote_port = port  
 
